e3cd2cf167b52c21333469681f78379c6a539557
ttanno <a20232524@pucp.edu.pe>
2025-03-11 08:59:40 -0500

    Third exercise

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,53 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
-    }
+        int sentinel = -1;//This is for the strong or extra requirements
+        while (sentinel != 0)
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 100);
+            int guess = -1;
+            int tries = 0;
+            while (guess != number)
+            {
+                Console.Write("What is your guess?");
+                string guess_word = Console.ReadLine();
+                guess = int.Parse(guess_word);
+                if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+                tries++;
+            }
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"It took you {tries} to guess");
+            sentinel = -1;//is this the unique way to reinitialized?
+
+            while (sentinel == -1)
+            {
+                Console.WriteLine("Do you want to continue?");
+                string intention = Console.ReadLine().ToLower();
+                if (intention == "yes")
+                {
+                    sentinel = 1;
+                }
+                else if (intention == "no")
+                {
+                    sentinel = 0;
+                }
+                else
+                {
+                    Console.WriteLine("Enter a valid answer");
+                }
+            }
+        }
+    }    
 }
