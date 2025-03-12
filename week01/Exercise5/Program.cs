f2cd52d01dca1589d296ac172391e471d6a254a5
ttanno <a20232524@pucp.edu.pe>
2025-03-12 01:22:32 -0500

    fifth exercise

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -2,8 +2,38 @@ using System;
 
 class Program
 {
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string PromptUserName()
+    {
+        Console.WriteLine("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("Please enter your favorite number: ");
+        string num = Console.ReadLine();
+        int number = int.Parse(num);
+        return number;
+    }
+    static int SquareNumber(int number)
+    {
+        int SquaredNumber = number * number;
+        return SquaredNumber;
+    }
+    static void DisplayResult(string name,int SquaredNumber)
+    {
+        Console.WriteLine($"Brother {name}, the square of your number is {SquaredNumber}");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int SquaredNumber = SquareNumber(number);
+        DisplayResult(name, SquaredNumber);
     }
 }
