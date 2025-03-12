546688d2e5ac05a3f755bbc0d73129ac7be6f75a
ttanno <a20232524@pucp.edu.pe>
2025-03-12 01:01:40 -0500

    Fourth exercise

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,70 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int number;
+        int sum = 0;
+
+        while (true)
+        {
+            Console.Write("Enter number: ");
+            string num = Console.ReadLine();
+
+            if (!int.TryParse(num, out number))
+            {
+                Console.WriteLine("Please enter a valid integer.");
+                continue;
+            }
+
+            if (number == 0)
+            {
+                break;
+            }
+
+            numbers.Add(number);
+            sum += number;
+        }
+
+        if (numbers.Count == 0)
+        {
+            Console.WriteLine("No numbers were entered.");
+            return;
+        }
+
+        float average = (float)sum / numbers.Count;
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+        foreach (int numInList in numbers)
+        {
+            if (numInList > max)
+            {
+                max = numInList;
+            }
+        }
+        Console.WriteLine($"The largest number is: {max}");
+//extra
+                int min = numbers[0];
+        foreach (int numInList in numbers)
+        {
+            if (numInList < min &&numInList>0)
+            {
+                min = numInList;
+            }
+        }
+        Console.WriteLine($"The smallest positive number is:: {min}");
+//extra
+        Console.WriteLine("The sorted list is:");
+        foreach (int numInList in numbers)
+        {
+            Console.WriteLine(numInList);
+        }
     }
 }
