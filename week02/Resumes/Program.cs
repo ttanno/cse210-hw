7b4d56a5efcff492cd1dcdcb779f5a88eacfa0a9
ttanno <a20232524@pucp.edu.pe>
2025-03-13 22:10:28 -0500

    W02 Learning Activity Abstraction

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,29 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        //Console.WriteLine($"{job1._company}\n{job2._company}");
+        //job1.Display();
+        //job2.Display();
+
+        Resume myResume = new Resume();
+        myResume._name = "Alisson Rose";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
